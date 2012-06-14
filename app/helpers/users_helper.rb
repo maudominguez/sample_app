8208920badc9e7988e817c9c3dc5eb6f0d175e35
Mauricio Dominguez <mauricio.dar@gmail.com>
2012-06-14 14:26:24 -0500

    Finish sign in

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,6 +1,6 @@
 module UsersHelper
 
-  def gravatar_for(user)
+  def gravatar_for(user, hash = {size: 40})
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
     gravatar_url = "http://gravatar.com/avatar/#{gravatar_id}"
     image_tag(gravatar_url, alt: user.name, class: "gravatar")
